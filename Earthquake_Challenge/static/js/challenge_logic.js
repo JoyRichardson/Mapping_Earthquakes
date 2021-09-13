fdec7ea0db5865d230ba3f80f809f21d1779e71d
JoyRichardson <jkr1965@gmail.com>
2021-09-12 20:37:22 -0500

    Commit Deliverable 1- final

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -142,7 +142,7 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json("https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json").then(function(platedata) {
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(platedata) {
     // Adding our geoJSON data, along with style information, to the tectonicplates layer.
       L.geoJson(platedata, {
         color: "#ff65f5",
